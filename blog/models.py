7046fcfdaef895ee478a5b2987ee47bdfe9b6973
Daniel Gonzalez <ogonzalez29@users.noreply.github.com>
2015-07-10 14:31:57 -0500

    Add Categorias class

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,10 @@
 from django.db import models
+from django.contrib import admin
+from tinymce import models as tinymce_models
+from PIL import Image
+from django.contrib.auth.models import User
 
+ESTADO_VISIBLE = [1,2]
 
 class Post(models.Model):
     title = models.CharField(max_length = 140)
@@ -10,12 +15,18 @@ class Post(models.Model):
     def __unicode__(self):
         return self.title
 
-class Category(models.Model):
-    name = models.CharField(
-        max_length=50
-        )
-    amount = models.DecimalField(
-        max_digits=10, 
-        decimal_places=2, 
-        default=0
-        )
+class Categorias(models.Model):
+    name = models.CharField(max_length=50)
+    amount = models.DecimalField(max_digits=10, decimal_places=2, default=0)
+    slug = models.SlugField(max_length=50, unique=True, help_text='Esto es para la URL.')
+    descripcion = models.TextField()
+    creada_en = models.DateTimeField(auto_now_add=True)
+    actualizada_al = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        db_table = 'categorias'
+        ordering = ['-creada_en']
+        verbose_name_plural = 'Categorias'
+    def __unicode__(self):
+        return self.nombre 
+
